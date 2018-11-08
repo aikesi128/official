dbb6b0da3277511fcef0c838ac34bd7315c0374d
469087843@qq.com <469087843@qq.com>
2018-11-08 18:20:45 +0800

    index页面开始

--- js/index.js
+++ js/index.js
@@ -1,25 +1,9 @@
 
 	
+	//入口函数
 	$(function(){
 		
-		$(".video img").click(function(){
 		 
-		//点击开始播放视频
-		var video = $(".video video");
-		
-		video.css("display","block");
-		
-		video.css("autoplay","autoplay");
-		
-		
-		video.onended = function(){
-			
-			$(this).css("display","none");
-			
-			
-		}
-		
-		});
 	
 		
 		
